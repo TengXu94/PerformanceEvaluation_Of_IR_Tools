54352f613266b69c48d1a51193d6ad4c77ff6bb6
TengXu94 <andxu24@gmail.com>
2017-04-02 14:36:00 +0200

    Update for nMDCG
    average nmdcg

--- Tester.py
+++ Tester.py
@@ -5,5 +5,10 @@ sfm = ScorerFileManager.ScorerFileManager()
 scorer_to_test = sfm.get_out_cran_def_countscorer()
 avg_maker = AverageRPrecision.AverageRPrecision()
 
+#avg_maker_nmdcg = AverageNMDCG.AverageNMDCGPrecision()
+
 print(avg_maker.compute_average_r_precision(scorer_to_test))
+
+#nMDCG
+#print(avg_maker_nmdcg.averagenMDCG(avg_maker_nmdcg.nMDCG_evaluation(scorer_to_test)))
 
